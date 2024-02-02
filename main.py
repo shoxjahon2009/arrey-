86a3c226dfcf46e286a2e0ed24a714c9f55009d3
Пользователь <shohjaxondehqonboyev59@gmail.com>
2024-02-02 22:16:39 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,92 @@
+
+# 1-misol
+# def table():
+#     table = [5,8,9,1,2]
+#     table.sort()
+#     print(table)
+# table()
+
+
+# 2-misol
+# def orqaga():
+#     orqaga = [7,8,11,66]
+#     orqaga.reverse()
+#     print(orqaga)
+# orqaga()
+
+# 3-misol
+# def calc():
+#     a = [1,5,6,8,3]
+#     resault = 1
+#     for s in a:
+#         resault = resault + s
+#     print(resault)
+# calc()
+
+
+#4-misol
+# nums = ["G","S","N"]
+# def connect():
+#     for x in nums:
+#         f = x + "7"
+#         print(f)
+# connect()
+
+
+#  # 5misol
+# # def toq_sonni_topish(royxat):
+# #         natija = 0
+# #         for son in royxat:
+# #             natija ^= son
+# #
+# #         return natija
+# # #
+# # royxatim = [1, 2, 3, 2, 1, 3, 4, 5, 4, ]
+# # natija = toq_sonni_topish(royxatim)
+# # print(f"Toq sonlar: {natija}")
+#
+# def myonlyson(table):
+#     resault = 0
+#     for x in table:
+#         resault^=x
+#     return resault
+# table = [3, 7, 9, ]
+# resault = myonlyson(table)
+# print(f"toq sonlar {resault}")
+
+
+# 6 misol
+# def even_num(b):
+#     for d in range(b + 1):
+#         if d % 2 == 0:
+#             print(d)
+#     return
+# num = 10
+# a = even_num(num)
+# print(a)
+
+# 6-misolni :2chisi
+# num_juft = int(input("son :"))  # boshlandi
+# num_toq = int(input("son :"))
+#
+#
+# def calc(number_juft, number_toq):
+#     print(f" juft son : {number_juft}\n toq son : {number_toq}")
+#
+#
+# calc(num_juft , num_toq)
+
+# 7 misol
+# a = [1, 2, 3, 4, 5, 6, 7, 8]
+# b =a[1]
+# d = a[2:-1]
+# c = a[-1]
+#
+# print(a)
+# print(b)
+# print(d)
+# print(c)
+
+
+
+
